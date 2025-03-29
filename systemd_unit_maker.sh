f688c3048c916e7bfdba3b387e84e2f1f8ecb043
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-03-29 11:55:29 +0100

    first draft
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- systemd_unit_maker.sh
+++ systemd_unit_maker.sh
@@ -1,15 +1,24 @@
 #!/bin/zsh
-
+#
+# usage section here
+#
 # parse args to get:
 # - a unit name
+# - a command
 # - --user or --system
-# --description
-# --freq (optional)
+# - --description
+# - --frequency
 #
 # replace each space by a _ in that unit_name
 #
-# copy the file next to this script called templated.service to ~/.config/systemd/user if not root or /etc/systemd/system otherwise with filename $unit_name + ".service" and do the same for the .timer template
+# copy the file next to this script called ./templates/templated.service to ~/.config/systemd/user if not root or /etc/systemd/system otherwise with filename $unit_name + ".service" and do the same for the .timer template
 #
 # then to those new files use sed to replace [[DESCRIPTION]] by $description given by arg
 #
-# then to those new files use sed to replace [[frequency]] by $description given by arg
+# then to those new files use sed to replace [[FREQUENCY]] by $frequency given by arg
+#
+# then to those new files use sed to replace [[COMMAND]] by $command given by arg
+#
+# then run systemctl --user daemon-reload or the sudo non user version if root
+#
+# then ask the user to press enter to run systemctl start on that service file (with or without --user)
